2e3c90c861a096f85cf7e29e6d61a52db337ecbd
mutaman97 <mutamanelhadi97@gmail.com>
2024-06-30 22:27:36 +0400

    fix: footer logo

--- resources/views/components/ui/logo.blade.php
+++ resources/views/components/ui/logo.blade.php
@@ -1,6 +1,5 @@
 @props(['width' => 'w-10', 'height' => 'h-10', 'color' => ''])
 
-<?xml version="1.0" encoding="utf-8"?>
     <!-- Generator: Adobe Illustrator 23.0.5, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
 <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
      width="31.06px" height="30.16px" viewBox="0 0 31.06 30.16" style="enable-background:new 0 0 31.06 30.16;" xml:space="preserve"
