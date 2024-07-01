a707f9012ac3817640e8f27609ed3235358091f8
mutaman97 <mutamanelhadi97@gmail.com>
2024-07-01 14:09:33 +0400

    fix resume file

--- resources/views/components/about/minfo-section.blade.php
+++ resources/views/components/about/minfo-section.blade.php
@@ -33,7 +33,7 @@
                 @foreach($skills as $skill)
                 <a href="#"
                    class="inline-block px-3.5 py-2 transition duration-300 border border-dashed text-black dark:text-white/70 border-platinum dark:border-greyBlack rounded-3xl md:px-5 md: md:py-2 hover:text-theme dark:hover:text-white">
-                    {{ $skill->name }} ({{ $skill->rate }})
+                    {{ $skill->name }} ({{ $skill->rate }}%)
                 </a>
                 @endforeach
 {{--                <a href="#"--}}
